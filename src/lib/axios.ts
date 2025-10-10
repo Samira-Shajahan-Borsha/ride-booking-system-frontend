e386f7a25355a006e8b572c455110bdd51eaa021
unknown <samiraborsha08@gmail.com>
2025-10-10 13:19:35 +0600

    feat: implemented refresh token functionality to handle multiple request failed

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,5 @@
 import config from "@/config";
-import axios from "axios";
+import axios, { type AxiosRequestConfig } from "axios";
 
 export const axiosInstance = axios.create({
     baseURL: config.baseUrl,
@@ -10,7 +10,7 @@ export const axiosInstance = axios.create({
 axiosInstance.interceptors.request.use(
     function (config) {
         // Do something before request is sent
-        console.log("Axios Request Config", config);
+        // console.log("Axios Request Config", config);
         return config;
     },
     function (error) {
@@ -19,17 +19,76 @@ axiosInstance.interceptors.request.use(
     }
 );
 
+interface pendingQueueItem {
+    resolve: (value: unknown) => void;
+    reject: (value: unknown) => void;
+}
+
+let isRefreshing = false;
+
+let pendingQueueItem: pendingQueueItem[] = [];
+
+const processQueue = (error: unknown) => {
+    pendingQueueItem.forEach((promise) => {
+        if (error) {
+            promise.reject(error);
+        } else {
+            promise.resolve(null);
+        }
+    });
+
+    pendingQueueItem = [];
+};
+
 // Add a response interceptor
 axiosInstance.interceptors.response.use(
-    function onFulfilled(response) {
-        // Any status code that lie within the range of 2xx cause this function to trigger
-        // Do something with response data
-        console.log("Axios Response", response);
+    (response) => {
         return response;
     },
-    function onRejected(error) {
-        // Any status codes that falls outside the range of 2xx cause this function to trigger
-        // Do something with response error
+    async (error) => {
+        // console.log("Request Failed", error.response);
+
+        const originalRequest = error.config as AxiosRequestConfig & {
+            _retry: boolean;
+        };
+
+        // console.log(originalRequest);
+
+        if (
+            error.response.status === 500 &&
+            error.response.data.message === "jwt expired" &&
+            !originalRequest._retry
+        ) {
+            console.log("Your token is expired");
+
+            originalRequest._retry = true;
+
+            if (isRefreshing) {
+                return new Promise((resolve, reject) => {
+                    pendingQueueItem.push({ resolve, reject });
+                })
+                    .then(() => axiosInstance(originalRequest))
+                    .catch((error) => Promise.reject(error));
+            }
+
+            isRefreshing = true;
+
+            try {
+                const response = await axiosInstance.post("/auth/refresh-token");
+                console.log(response, "New Token");
+
+                processQueue(null);
+
+                return axiosInstance(originalRequest);
+            } catch (error) {
+                processQueue(error);
+                return Promise.reject(error);
+            } finally {
+                isRefreshing = false;
+            }
+        }
+
+        // For everything
         return Promise.reject(error);
     }
 );
